bb5d60d9008d59bbd80c3cef549229a71ebc49c8
Oleksii Horhcynskyi <pan.alexeu@gmail.com>
2025-05-14 20:10:38 +0300

    defined async tests for async openai model methods

--- tests/integr.py
+++ tests/integr.py
@@ -1,11 +1,13 @@
 import pytest
 from dotenv import load_dotenv
-from openai import OpenAI
+from openai import OpenAI, AsyncOpenAI
 from pydantic import BaseModel
 from rich import print
 
 from asgm.models.openai import OpenAIModel, Tool
 
+
+# ==== Fixtures ====
 
 @pytest.fixture(scope='module')
 def model():
@@ -16,34 +18,21 @@ def model():
 
 
 @pytest.fixture(scope='module')
+def async_model():
+    load_dotenv()
+    client = AsyncOpenAI()
+    return OpenAIModel(client=client, model='gpt-4.1-nano')
+
+
+@pytest.fixture(scope='module')
 def input():
     return [
         {'role': 'user', 'content': 'What is Odessa? How many years have passed since foundation (cur. year 2025)?'}]
 
 
-def test_openai_model_returns_text_completion(model, input):
-    res = model.create_completion(input=input, temperature=0)
-    print(res)
-
-    assert 'Odessa' in res
-
-
-def test_openai_model_returns_structured_completion(model, input):
-    class Response(BaseModel):
-        headline: str
-        text: str
-
-    res = model.create_structured_completion(
-        input=input,
-        text_format=Response
-    )
-    print(res)
-
-    assert isinstance(res, Response)
-
-
-def test_openai_model_returns_tool_completion(model, input):
-    tools = [
+@pytest.fixture(scope='module')
+def tools():
+    return [
         Tool(
             name='web_search',
             func=lambda query: 'Odessa was officially founded on September 2, 1794.',
@@ -90,7 +79,66 @@ def test_openai_model_returns_tool_completion(model, input):
         )
     ]
 
+
+# ==== Tests ====
+
+def test_openai_model_returns_text_completion(model, input):
+    res = model.create_completion(input=input, temperature=0)
+    print(res)
+
+    assert 'Odessa' in res
+
+
+def test_openai_model_returns_structured_completion(model, input):
+    class Response(BaseModel):
+        headline: str
+        text: str
+
+    res = model.create_structured_completion(
+        input=input,
+        text_format=Response
+    )
+    print(res)
+
+    assert isinstance(res, Response)
+
+
+def test_openai_model_returns_tool_completion(model, input, tools):
     res = model.create_tool_completion(
+        input=input,
+        tools=tools
+    )
+    print(res)
+
+    # model called web_search tool and calculator tool
+    assert len(res) == 2
+
+
+# ==== Async Tests ====
+
+async def test_async_openai_model_returns_text_completion(async_model, input):
+    res = await async_model.acreate_completion(input=input, temperature=0)
+    print(res)
+
+    assert 'Odessa' in res
+
+
+async def test_async_openai_model_returns_structured_completion(async_model, input):
+    class Response(BaseModel):
+        headline: str
+        text: str
+
+    res = await async_model.acreate_structured_completion(
+        input=input,
+        text_format=Response
+    )
+    print(res)
+
+    assert isinstance(res, Response)
+
+
+async def test_async_openai_model_returns_tool_completion(async_model, input, tools):
+    res = await async_model.acreate_tool_completion(
         input=input,
         tools=tools
     )
