928b9445b54fba2ee4ae279f270212202ef37ff5
Oleksii Horhcynskyi <pan.alexeu@gmail.com>
2025-05-15 19:29:42 +0300

    implemented fake chat model

---
+++ asgm/models/fake.py
@@ -0,0 +1,33 @@
+from typing import Any, Type
+
+from pydantic import BaseModel
+
+from .base_model import BaseChatModel
+from .types import Message, Tool
+
+
+class FakeChatModel(BaseChatModel):
+    def create_completion(self, input: list[Message], **kwargs) -> str:
+        return 'fake response'
+
+    async def acreate_completion(self, input: list[Message], **kwargs) -> str:
+        return 'fake response'
+
+    def create_tool_completion(self, input: list[Message], tools: list[Tool], **kwargs) -> list[Any]:
+        return [tool['func'](**kwargs) for tool in tools]
+
+    async def acreate_tool_completion(self, input: list[Message], tools: list[Tool], **kwargs) -> list[Any]:
+        return [tool['func'](**kwargs) for tool in tools]
+
+    def create_structured_completion(self, input: list[Message], text_format: Type[BaseModel], **kwargs) -> BaseModel:
+        return text_format(**kwargs)
+
+    async def acreate_structured_completion(
+            self,
+            input: list[Message],
+            text_format: Type[BaseModel],
+            **kwargs
+    ) -> BaseModel:
+        return text_format(**kwargs)
+
+
